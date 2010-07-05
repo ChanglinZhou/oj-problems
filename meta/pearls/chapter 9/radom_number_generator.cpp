cd9cd049df2a3d7ca60e795aa5ffa1ccdfd6d697
carmack.shi <carmack.shi@localhost>
2010-07-06 00:28:58 +0800

    Modify code from chapter 9, programming pearls II.

--- meta/pearls/chapter 9/radom_number_generator.cpp
+++ meta/pearls/chapter 9/radom_number_generator.cpp
@@ -1,96 +1,173 @@
-/* Copyright (C) 1999 Lucent Technologies */
-/* From 'Programming Pearls' by Jon Bentley */
-
-/* genbins.c -- generate random numbers with bins */
-
-/* If NODESIZE is 8, this program uses the special-case malloc.
-Change NODESIZE to 0 to use the system malloc.
+/* Copyright (C) 1999 Lucent Technologies
+ * Author: Jon Bentley
+ * Purpose: Generate random numbers with different policies.
+ * Modified by: Su Shi(carmack.shi@gmail.com)
 */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstddef>
+#include <cassert>
+using namespace std;
 
-#define NODESIZE 8
-#define NODEGROUP 1000
+// If NODESIZE is 8, this program uses the special-case malloc.
+// Change NODESIZE to 0 to use the system malloc.
+const int kBlockSize = 0;
+const int kBlockGroup = 1000;
 
-int nodesleft = 0;
-char* freenode;
-
-void* pmalloc(int size) {
-  void* p;
-  if (size != NODESIZE)
-    return malloc(size);
-  if (nodesleft == 0) {
-    freenode = static_cast<char*>(malloc(NODEGROUP*NODESIZE));
-    nodesleft = NODEGROUP;
-  }
-  nodesleft--;
-  p = static_cast<void*>(freenode);
-  freenode += NODESIZE;
-  return p;
-}
-
-struct node {
-  int val;
-  struct node *next;
+union Block {
+  Block* next;
+  char data_trunk[kBlockSize];
 };
 
-struct node **bin, *sentinel;
-int bins, bincnt, maxval;
+Block* free_block_head = NULL;
 
-void initbins(int maxelms, int pmaxval) {
-  int i;
-  bins = maxelms;
-  maxval = pmaxval;
-  bin = static_cast<struct node**>(pmalloc(bins*sizeof(struct node *)));
-  sentinel = static_cast<struct node*>(pmalloc(sizeof(struct node)));
-  sentinel->val = maxval;
-  for (i = 0; i < bins; i++)
-    bin[i] = sentinel;
-  bincnt = 0;
-}
+void* PoolMalloc(size_t size) {
+  void* p = NULL;
 
-struct node *rinsert(struct node *p, int t) {
-  if (p->val < t) {
-    p->next = rinsert(p->next, t);
-  } else if (p->val > t) {
-    struct node *q = static_cast<struct node*>(pmalloc(sizeof(struct node)));
-    q->val = t;
-    q->next = p;
-    p = q;
-    bincnt++;
+  if (size != kBlockSize)
+    return malloc(size);
+
+  if (free_block_head == NULL) {
+    free_block_head = static_cast<Block*>(malloc(kBlockGroup*kBlockSize));
+
+    if (free_block_head) {
+      for (size_t i = 0; i < kBlockGroup - 1; ++i) {
+        free_block_head[i].next = free_block_head + i + 1;
+      }
+      free_block_head[kBlockGroup - 1].next = NULL;
+    }
   }
+
+  if (free_block_head) {
+    p = free_block_head;
+    free_block_head = free_block_head->next;
+  }
+
   return p;
 }
 
-void insert(int t) {
-  int i;
-  i = t / (1 + maxval/bins);
-  i = t / (1 + maxval/bins);
-  bin[i] = rinsert(bin[i], t);
+void PoolFree(void* memory, size_t size = kBlockSize) {
+  if (size != kBlockSize)
+    return free(memory);
+
+  Block* block = static_cast<Block*>(memory);
+  block->next = free_block_head;
+  free_block_head = block;
 }
 
-void report() {
-  int i, j = 0;
-  struct node *p;
-  for (i = 0; i < bins; i++)
-    for (p = bin[i]; p != sentinel; p = p->next)
-      printf("%d\n", p->val);
-  /* Uncomment for testing, comment for profiling */
-}
 
-int bigrand() {
+int BigRand() {
   return RAND_MAX*rand() + rand();
 }
 
-int main(int argc, char *argv[]) {
-  int m = atoi(argv[1]);
-  int n = atoi(argv[2]);
 
-  initbins(m, n);
-  while (bincnt < m) {
-    insert(bigrand() % n);
+class IntSetBins {
+ public:
+  IntSetBins(size_t max_elements_number, size_t max_value)
+    : elements_number_(0), 
+      bins_number_(max_elements_number),
+      max_value_(max_value) {
+    bins_ = static_cast<Node**>(PoolMalloc(bins_number_*sizeof(Node*)));
+    sentinel_ = static_cast<Node*>(PoolMalloc(sizeof(Node)));
+    sentinel_->value_ = max_value_;
+    sentinel_->next_ = NULL;
+    for (size_t i = 0; i < bins_number_; ++i)
+      bins_[i] = sentinel_;
   }
-  report();
+  ~IntSetBins() {
+    for (size_t i = 0; i < bins_number_; ++i)
+      for (Node* node = bins_[i]; node != sentinel_; node = node->next_)
+        PoolFree(node);
+
+    PoolFree(bins_);
+    PoolFree(sentinel_);
+  }
+
+  void Insert(size_t value) {
+    int hash_index = value / (1 + max_value_ / bins_number_);
+    //bins_[hash_index] = RecursiveSubInsert(bins_[hash_index], value);
+    bins_[hash_index] = NonRecursiveSubInsert(bins_[hash_index], value);
+  }
+
+  void Report() {
+    for (size_t i = 0; i < bins_number_; ++i)
+      for (Node* node = bins_[i]; node != sentinel_; node = node->next_)
+        /*printf("%d\n", node->value_)*/;
+  }
+
+  size_t size() const {
+    return elements_number_;
+  }
+
+ private:
+  struct Node {
+    size_t value_;
+    Node *next_;
+  };
+
+  Node* RecursiveSubInsert(Node* root, size_t value) {
+    assert(root);
+
+    if (root->value_ < value) {
+      root->next_ = RecursiveSubInsert(root->next_, value);
+    } else if (root->value_ > value) {
+      Node* new_node = static_cast<Node*>(PoolMalloc(sizeof(Node)));
+      new_node->value_ = value;
+      new_node->next_ = root;
+      root = new_node;
+      ++elements_number_;
+    }
+
+    return root;
+  }
+
+  Node* NonRecursiveSubInsert(Node* root, size_t value) {
+    assert(root);
+
+    Node *prev = NULL, *current = root;
+    while (current->value_ < value) {
+      prev = current;
+      current = current->next_;
+    }
+
+    if (current->value_ > value) {
+      Node* new_node = static_cast<Node*>(PoolMalloc(sizeof(Node)));
+      new_node->value_ = value;
+      new_node->next_ = current;
+      if (prev)
+        prev->next_ = new_node;
+      else
+        root = new_node;
+      ++elements_number_;
+    }
+
+    return root;
+  }
+
+  size_t elements_number_;
+  size_t bins_number_;
+  size_t max_value_;
+  Node** bins_;
+  Node* sentinel_;
+};
+
+
+int main(int argc, char *argv[]) {
+  size_t max_elements = 10000;
+  size_t max_value = 1000000;
+
+  if (argc >= 3) {
+    max_elements = atoi(argv[1]);
+    max_value = atoi(argv[2]);
+  }
+
+  // Integer set with bins.
+  IntSetBins int_set_bins(max_elements, max_value);
+  while (int_set_bins.size() < max_elements) {
+    int_set_bins.Insert(BigRand() % max_value);
+  }
+  int_set_bins.Report();
+
   return 0;
 }
